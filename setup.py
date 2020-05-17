972a4a1d7f92307dd0260649b965d3ea3b6a30f0
Daniel Rozenberg <me@danielrozenberg.com>
2020-05-17 16:48:50 -0400

    Python 3.5 is unsupported

--- setup.py
+++ setup.py
@@ -23,11 +23,10 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Typing :: Typed',
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
 )
